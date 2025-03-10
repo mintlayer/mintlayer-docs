8280e03a456d2ee0c58a24857fcc954e1b355ca5
Sergey Chystiakov <anyxem@gmail.com>
2025-03-10 12:51:26 +0100

    feat: update homepage icons

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -11,32 +11,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'What is Mintlayer?',
+    Svg: require('@site/static/img/feature-extension-icon.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Learn more about Mintlayer and how it can help you build your next blockchain project.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Documentation',
+    Svg: require('@site/static/img/feature_decentralised-icon.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Find the information you need to start building on Mintlayer.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'User Guide',
+    Svg: require('@site/static/img/feature-rocket-icon.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Learn how to use Mintlayer products and services.
       </>
     ),
   },
