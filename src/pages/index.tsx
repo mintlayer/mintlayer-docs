d8f2c0742561c71e65369d1dd5318cbe3abcb72f
Sergey Chystiakov <anyxem@gmail.com>
2025-03-10 12:56:25 +0100

    feat: update title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,8 +26,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Mintlayer documentation portal">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
